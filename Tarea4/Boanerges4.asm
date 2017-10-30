92b35cbc5b38ac7e8dee4300afc3deac003cb775
Boanerges <boanerges.martinez@ucr.ac.cr>
2017-10-30 13:49:12 -0600

    LEER TECLAS listo

--- Tarea4/Boanerges4.asm
+++ Tarea4/Boanerges4.asm
@@ -27,15 +27,17 @@
 ;                         ESTRUCTURAS DE DATOS GLOBALES
 ;...............................................................................
         org $1000
-PATRON ds 1  ;variable PATRON, indice de control del for-next iterativo4
-REB ds 1     ;variable REB se utilizara para contar los rebotes
-TECLA ds 1  ;TECL_LISTA:TECLA, Bandera y valor de la tecla ingresado, nos permitira saber si se leyo un valor valido desde el teclado
-BUFFER ds 1  ;definimos las pariables para el buffer
-TMP1 ds 1    ;definimos las variables temporales que utilizaremos para validar y
-TMP2 ds 1    ;cargar los valores a PATRON
-TMP3 ds 1
+PATRON: ds 1  ;variable PATRON, indice de control del for-next iterativo4
+REB: ds 1     ;variable REB se utilizara para contar los rebotes
+BANDERAS: ds 1   ;BANDERAS = $0:%0:%PRIMERA:%VALIDA:%TECL_LISTA
+TECLA: ds 1  ;TECLA valor de la tecla ingresado
+BUFFER: ds 1  ;definimos las pariables para el buffer
+TMP1: ds 1    ;definimos las variables temporales que utilizaremos para validar y
+TMP2: ds 1    ;cargar los valores a PATRON
+TMP3: ds 1
+VALOR:  ds 1    ;variable utilizada por subrutina TECLADO y LEDS
         org $1010
-TECLAS db $01,$02,$03,$04,$05,$06,$07,$08,$09,$B,$0,$E ;se define la tabla TECLAS
+TECLAS: db $01,$02,$03,$04,$05,$06,$07,$08,$09,$B,$0,$E ;se define la tabla TECLAS
 ;...............................................................................
 
 ;*******************************************************************************
@@ -58,7 +60,7 @@ TECLAS db $01,$02,$03,$04,$05,$06,$07,$08,$09,$B,$0,$E ;se define la tabla TECLA
         Bset DDRJ, $02   ;posicion 1 PORTJ como salida
         Bclr PTJ, $02    ;habilita los LEDS, se pone en 0 posicion 1 PORTJ
         ;configuracion RTI
-        movb #$80,CRGINT ;Habilita(ENABLE) la interrupción de RTI
+        movb #$80,CRGINT ;Habilita(ENABLE) la interrupcion de RTI
         movb #$40,RTICTL ;#$MN, M=4, N=0, Tic_RTI = 1.024ms
         ;PORTA, parte alta como salidas, parte baja como entradas
         movb #$F0,DDRA
@@ -78,9 +80,12 @@ TECLAS db $01,$02,$03,$04,$05,$06,$07,$08,$09,$B,$0,$E ;se define la tabla TECLA
         Movb #$FF,TMP1
         Movb #$FF,TMP2
         Movb #$FF,TMP3
+        Movb #$FF,VALOR
         Movb #0,PORTB  ;apaga los leds
-        ;Wai
-Espere  Brclr TECLA,$10,Espere  ;se revisa el valor de TECL_LISTA en el nible mas significativo de TECLA
+Espere  Wai             ;espera que ocurra la interrupcion RTI
+        ;BANDERAS = $0:%0:%Primera:%VALIDA:%TECL_LISTA
+        ;BANDERAS, PRIMERA($04), VALIDA($02), TECL_LISTA($01)
+        Brclr BANDERAS,$01,Espere  ;se revisa el valor de TECL_LISTA en el nible mas significativo de TECLA
         Jsr TECLADO     ;se llama a la subrutina TECLADO
         Brset VALOR,$FF,Espere ;si VALOR =$FF, no ha habido cambios en VALOR
         Jsr LEDS        ;se llama a la subrutina TECLADO, si ha habido cambios en VALOR
@@ -97,11 +102,15 @@ Espere  Brclr TECLA,$10,Espere  ;se revisa el valor de TECL_LISTA en el nible ma
 ;...............................................................................
 ;             Declaracion de variables locales
 ;...............................................................................
-VALOR: DS 1
+;VALOR: DS 1
 Va: DS 1
 ;...............................................................................
 ;...............................................................................
-TECLADO rts
+TECLADO Movb TECLA,VALOR
+        Movb $FF,TECLA
+        ;BANDERAS, PRIMERA($04), VALIDA($02), TECL_LISTA($01)
+        Bclr Banderas,$01
+        Rts
 
 
 ;...............................................................................
@@ -113,11 +122,13 @@ TECLADO rts
 
 ;...............................................................................
 ;...............................................................................
-LEDS rts
+LEDS    Movb VALOR,PORTB
+        Movb $FF,VALOR
+        rts
 
  
 ;*******************************************************************************
-;              SUBRUTINA DE SERVICIO RTI
+;              SUBRUTINA DE SERVICIO RTI_ISR
 ;*******************************************************************************
 ; Esta subrutina es la encargada de leer el teclado, y revisar si hubo o hubieron,
 ; teclas validas, ademas se encarga del manejo de rebote.
@@ -127,22 +138,27 @@ LEDS rts
 
 ;...............................................................................
 ;...............................................................................
-RTI_ISR Ldaa #$EF
+RTI_ISR ;falta check REB,BUFFER=FF
+	Ldaa #$EF       ;inicia LEER TECLA
+        Staa PORTA
         Clrb
-        Movb #4, PATRON
-forNext Staa PORTA
-	incb
-        Brclr PORTA,$01,teclaIN        ;revisa primer patron
+        Movb #$04, PATRON
+forNext incb
+        Brclr PORTA,$01,teclaIn        ;revisa primer patron
         incb
-        Brclr PORTA,$02,teclaIN        ;revisa segundo patron
+        Brclr PORTA,$02,teclaIn        ;revisa segundo patron
         incb
-        Brclr PORTA,$04,teclaIN        ;revisa tercer patron
+        Brclr PORTA,$04,teclaIn        ;revisa tercer patron
         Sec                 ;c=1
-        Rora    ;c->R1->c, ratos para meter los nuevos valores de prueba
+        Rola    ;c<-R1<-c, rator para meter los nuevos valores de prueba de entrada a PORTA
         Staa PORTA
         Dec PATRON
-        Bne forNext
-teclaIN Ldx TECLAS
-        Movb B,X, PORTB
-        Bset CRGFLG,#%10000000                ;resetea la bandera
+        Bne forNext     ;revisa si PATRON!=0, salta al forNext
+        Bra CHECKBUF	;check BUFFER=$FF
+teclaIn Ldx TECLAS      ;Encontro tecla, la guarda en BUFFER
+        Ldaa B,X
+        Staa BUFFER     ;termina LEER TECLA
+CHECKBUF
+        
+salida  Bset CRGFLG,$80                ;resetea la bandera
         RTI
