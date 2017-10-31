f64ecf35b6865c3711b2e46300f7e0f31ca9f2e4
Boanerges <boanerges.martinez@ucr.ac.cr>
2017-10-31 03:28:01 -0600

    limpieza del codigo, wai funciona

--- Tarea4/Boanerges4.asm
+++ Tarea4/Boanerges4.asm
@@ -84,7 +84,7 @@ TECLAS: db $01,$02,$03,$04,$05,$06,$07,$08,$09,$B,$0,$E ;se define la tabla TECL
         Movb #$FF,TMP2
         Movb #$FF,TMP3
         Movb #$00,PORTB  ;apaga los leds
-Espere  ;Wai             ;espera que ocurra la interrupcion RTI
+Espere  Wai             ;espera que ocurra la interrupcion RTI
         ;BANDERAS = $0:%0:%Primera:%VALIDA:%TECL_LISTA
         ;BANDERAS, PRIMERA($04), VALIDA($02), TECL_LISTA($01)
         Brclr BANDERAS,$01,Espere  ;se revisa el valor de TECL_LISTA en el nible mas significativo de TECLA
@@ -140,10 +140,7 @@ LEDS    Movb VALOR,PORTB
 Contador: ds 1  ;respaldo del conteo en R2(B)
 ;...............................................................................
 ;...............................................................................
-RTI_ISR Brclr REB,$FF,LEER_T
-        ;Ldab REB        ;Revisa si rebote es cero, sino lo decrementa
-        ;Cmpb #$00
-        ;Beq LEER_T
+RTI_ISR Brclr REB,$FF,LEER_T ;Revisa si rebote es cero, sino lo decrementa
         Dec REB
         LBra salida
 LEER_T  Movb #$FF,BUFFER
@@ -157,36 +154,30 @@ forNext incb
         Brclr PORTA,$02,teclaIn        ;revisa segundo patron
         incb
         Brclr PORTA,$04,teclaIn        ;revisa tercer patron
-        ;Sec                 ;c=1
         Rola    ;c<-R1<-c, rator para meter los nuevos valores de prueba de entrada a PORTA
         Staa PORTA
         Dec PATRON
         Bne forNext     ;revisa si PATRON!=0, salta al forNext
         Bra CBUFF        ;check BUFFER=$FF
 teclaIn Ldx #TECLAS      ;Encontro tecla, la guarda en BUFFER
-        ;Stab Contador
-        ;dex             ;decrementa el puntero porq R2(A) cuenta desde 1
-        ;Leax b,X
-        decb
+        decb             ;decremente porq R2(A) cuenta desde 1
         Ldaa B,X
         Staa BUFFER     ;termina LEER TECLA, cargando en BUFFER usando el acumulador R2
-        ;Movb B,X,BUFFER        ;termina LEER TECLA, cargando en BUFFER usando el acumulador R2
-        ;Stab PORTB
-        ;Staa PORTB     ;termina LEER TECLA
 CBUFF   Brset BUFFER,$FF,PuntoE ;si BUFFER =$FF, Nohay tecla
         Bra PuntoBC
 PuntoE  Brset TECLA,$FF,salida ;PuntoE, si TECLA =$FF ha habido error de lectura
-        Brset BANDERAS,$02,PuntoA2
+        ;BANDERAS, PRIMERA($04), VALIDA($02), TECL_LISTA($01)
+        Brset BANDERAS,$02,PuntoA2;VALIDA=1?
         Ldaa BUFFER             ;TECLA=BUFFER?
         Cmpa TECLA
         Beq PuntoE1         ;TECLA=BUFFER
         Movb #$FF,TECLA        ;TECLA!=BUFFER
         Bra salida
-PuntoE1 Bset BANDERAS,$02
+PuntoE1 Bset BANDERAS,$02   ;VALIDA <- 1
         Bra salida
 PuntoA2 Brset BUFFER,$FF,PuntoA3;Tecla Liberada si BUFFER=$FF
         Bra salida
-PuntoA3 Clr BANDERAS   ;se limpian las BANDERAS y se pone TECL_LISTA=1
+PuntoA3 Clr BANDERAS   ;se limpian las BANDERAS y se pone TECL_LISTA <- 1
         Bset BANDERAS,$01
         Bra salida
         ;BANDERAS, PRIMERA($04), VALIDA($02), TECL_LISTA($01)
@@ -194,7 +185,6 @@ PuntoBC Brset BANDERAS,$04,PuntoE;primera!=0 se debe revisar TECLA para validarl
         Bset BANDERAS,$04        ;PRIMERA <- 1
         Movb #10,REB
         Movb BUFFER,TECLA
-        ;Movb BUFFER,PORTB
         Bra salida
 salida  Bset CRGFLG,$80                ;resetea la bandera
         RTI
